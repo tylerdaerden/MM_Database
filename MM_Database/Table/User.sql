0dfefad9c07cfdfcc2c5e39cdf60b2e028ba07c7
Tyler Daerden <daerdentyler@gmail.com>
2023-08-31 11:56:04 +0200

    add isadmin

--- MM_Database/Table/User.sql
+++ MM_Database/Table/User.sql
@@ -6,7 +6,8 @@
 	[UserName] NVARCHAR(50) NOT  NULL,
 	[Email] NVARCHAR(384) NOT NULL,
 	[BirthDate] DATE NOT NULL,
-	[Password] BINARY(64) NOT NULL
+	[Password] BINARY(64) NOT NULL, 
+	[IsAdmin] BIT NOT NULL DEFAULT 0
 
 	CONSTRAINT [PK_User] PRIMARY KEY ([Id]) ,
 	CONSTRAINT [UK_User_Email] UNIQUE ([Email]),
