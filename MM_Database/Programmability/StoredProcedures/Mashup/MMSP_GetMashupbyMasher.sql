aeb3520b17d7bb2ca4ecf6b3bc12cc4bb2899ed9
Tyler Daerden <daerdentyler@gmail.com>
2023-09-04 11:44:29 +0200

    Modif Update

--- MM_Database/Programmability/StoredProcedures/Mashup/MMSP_GetMashupbyMasher.sql
+++ MM_Database/Programmability/StoredProcedures/Mashup/MMSP_GetMashupbyMasher.sql
@@ -6,21 +6,5 @@ SELECT DISTINCT MasherName as [Masher] , mm.PathFile as [Mashup]
 FROM [dbo].Masher as m
 JOIN [Mashup] as mm on mm.MasherId = m.Id
 WHERE [MasherName] = @MasherName
---SELECT PathFile as [Mashups] 
---FROM [dbo].Mashup
-
 
 END
-
-
-
-
-
---CREATE PROCEDURE [dbo].[MMSP_GetMashupbyArtist]
---	@MashupArtist NVARCHAR(100)
---AS
---BEGIN
---SELECT DISTINCT PathFile as [Mashups] , t.ArtistName as [Artist]
---FROM [dbo].Mashup as m
---LEFT JOIN [Track] as t ON m.Id = t.ArtistId
---END
--- MM_Database/Programmability/StoredProcedures/Mashup/MMSP_UpdateMashup.sql
+++ MM_Database/Programmability/StoredProcedures/Mashup/MMSP_UpdateMashup.sql
@@ -1,7 +1,6 @@
-﻿CREATE PROCEDURE UpdateMashup
+﻿CREATE PROCEDURE [dbo].[MMSP_UpdateMashup]
     @MashupId INT,
     @Title NVARCHAR(80),
-    @Length INT,
     @PathFile NVARCHAR(384)
 AS
 BEGIN
@@ -9,13 +8,11 @@ BEGIN
     SET [Title] = @Title
     WHERE [Id] = @MashupId;
 END
-BEGIN
-    UPDATE [dbo].[Mashup]
-    SET [Length] = @Length
-    WHERE [Id] = @MashupId;
-END;
+
+
 BEGIN
     UPDATE [dbo].[Mashup]
     SET [PathFile] = @Pathfile
     WHERE [Id] = @MashupId;
 END
+
